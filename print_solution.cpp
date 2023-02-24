21bf2f8181333d4cfd7a4523e2febc8d243d1400
Dmitry <88dmitri04@gmail.com>
2023-02-24 20:25:54 +0300

    Minor updates

--- print_solution.cpp
+++ print_solution.cpp
@@ -1,22 +1,23 @@
 #include "print_solution.h"
+#include <assert.h>
 #include <stdio.h>
 
 void print_solution(solution solution)
 {
     switch (solution.flag) {
-    case (-1):
+    case (-1): // Why is it weird?)
         printf("\nNo solutions on the set of real numbers.\n");
         break;
     case (0):
         printf("\nx can take any value.\n");
         break;
     case (1):
-        printf("x = %lf\n", solution.x1);
+        printf("\nx = %lf\n", solution.x1);
         break;
     case (2):
-        printf("x = %lf or x = %lf\n", solution.x1, solution.x2);
+        printf("\nx = %lf or x = %lf\n", solution.x1, solution.x2);
         break;
     default:
-        printf("If you see this, you weren't supposed to...\n");
+        assert(true);
     }
 }
